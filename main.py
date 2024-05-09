a4b56f4dbcba3b1608fe27a95d647a689a2b3b08
qiehs <139407249+qiehs@users.noreply.github.com>
2024-05-09 11:01:36 -0400

    5/9
    Uploaded more rock variations, beginning to add moving backwards functioning. Randomized the rock generation positioning better so its not as densely placed.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dust import Dust
 from wasd import Wasd
 from dune import Dune
 from rock import Rock
+from rock2 import Rock2
 
 # set up pygame modules
 pygame.init()
@@ -20,6 +21,7 @@ size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 
 going_forward = False
+going_backward = False
 going_left = False
 going_right = False
 begin = False
@@ -36,8 +38,11 @@ d = Dust(700, 600)
 wasd = Wasd(800, 400)
 game_map = Dune(0, 0)
 
-rock = Rock(random.randint(0, 1440), -200)
-rock2 = Rock(random.randint(0, 1440), -150)
+rock = Rock(random.randint(0, 1440), -150)
+rock2 = Rock(random.randint(0, 1440), -150 - random.randint(0, 300))
+rock3 = Rock2(random.randint(0, 1440), -200)
+rock4 = Rock2(random.randint(0, 1440), -240 - random.randint(-20, 300))
+
 rock2.change_size(1.25)
 
 
@@ -62,6 +67,10 @@ while run:
         going_right = True
     else:
         going_right = False
+    if keys[pygame.K_s]:
+        going_backward = True
+    else:
+        going_backward = False
 
     # --- Main event loop
     # ----- NO BLIT ZONE START ----- #
@@ -74,6 +83,8 @@ while run:
     screen.blit(game_map.image, (game_map.x, game_map.y))
     screen.blit(rock.image, (rock.x, rock.y))
     screen.blit(rock2.image, (rock2.x, rock2.y))
+    screen.blit(rock3.image, (rock3.x, rock3.y))
+    screen.blit(rock4.image, (rock4.x, rock4.y))
     if begin is True:
         if going_forward:
             screen.blit(d.image, (car.x + 120 + random.randint(0, 20), car.y + 265 + random.randint(0, 20)))
@@ -81,12 +92,21 @@ while run:
             screen.blit(d.image, (car.x, car.y + 265 + random.randint(0, 20)))
             rock.move(rock.x, rock.y + 3)
             rock2.move(rock2.x, rock2.y + 3)
+            rock3.move(rock3.x, rock3.y + 3)
+            rock4.move(rock4.x, rock4.y + 3)
             if rock.y > 1000:
-                rock.y = -200
-                rock.move(random.randint(0, 1440), rock.y)
+                rock.y = -150
+                rock = Rock(random.randint(0, 1440), -150)
+                rock.change_size(random.randint(int(0.8), int(1.45)))
             if rock2.y > 1000:
-                rock2.y = -200
-                rock2.move(random.randint(0, 1440), rock2.y)
+                rock2 = Rock(random.randint(0, 1440), -150 - random.randint(0, 300))
+                rock2.change_size(random.randint(int(0.8), int(1.45)))
+            if rock3.y > 1000:
+                rock3.y = -150
+                rock3 = Rock2(random.randint(0, 1440), -200)
+            if rock4.y > 1000:
+                rock4 = Rock2(random.randint(0, 1440), -240 - random.randint(-20, 300))
+                rock4.change_size(random.randint(int(0.8), int(1.45)))
         if going_left and going_forward and car.x > 0:
             car.move(car.x - 3, car.y)
         if going_right and going_forward and car.x < 1445:
