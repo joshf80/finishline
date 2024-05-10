54010faaf1664a5342b0791e35b4bc770c911d79
qiehs <139407249+qiehs@users.noreply.github.com>
2024-05-10 11:03:45 -0400

    5/10
    worked on adding more rock generation, and caused issue with rocks disappearing before they are supposed to.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ my_font = pygame.font.SysFont('Comic Sans', 65)
 pygame.display.set_caption("Finishline")
 
 # set up variables for the display
-SCREEN_HEIGHT = 1000
+SCREEN_HEIGHT = 900
 SCREEN_WIDTH = 1600
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
@@ -38,10 +38,13 @@ d = Dust(700, 600)
 wasd = Wasd(800, 400)
 game_map = Dune(0, 0)
 
-rock = Rock(random.randint(0, 1440), -150)
-rock2 = Rock(random.randint(0, 1440), -150 - random.randint(0, 300))
-rock3 = Rock2(random.randint(0, 1440), -200)
-rock4 = Rock2(random.randint(0, 1440), -240 - random.randint(-20, 300))
+rand_shift = random.randint(-200, 200)
+rand_shift2 = random.randint(-200, 200)
+
+rock = Rock(random.randint(0, 1440), -250)
+rock2 = Rock(random.randint(0, 1440), -250 - random.randint(-75, 75))
+rock3 = Rock2(random.randint(0, 1440), -250)
+rock4 = Rock2(random.randint(0, 1440), -250 - random.randint(0, 300))
 
 rock2.change_size(1.25)
 
@@ -83,8 +86,10 @@ while run:
     screen.blit(game_map.image, (game_map.x, game_map.y))
     screen.blit(rock.image, (rock.x, rock.y))
     screen.blit(rock2.image, (rock2.x, rock2.y))
+    screen.blit(rock2.image, (rock2.x - rand_shift, rock2.y + 300))
     screen.blit(rock3.image, (rock3.x, rock3.y))
     screen.blit(rock4.image, (rock4.x, rock4.y))
+    screen.blit(rock4.image, (rock4.x + rand_shift2, rock4.y - 200))
     if begin is True:
         if going_forward:
             screen.blit(d.image, (car.x + 120 + random.randint(0, 20), car.y + 265 + random.randint(0, 20)))
@@ -94,23 +99,27 @@ while run:
             rock2.move(rock2.x, rock2.y + 3)
             rock3.move(rock3.x, rock3.y + 3)
             rock4.move(rock4.x, rock4.y + 3)
-            if rock.y > 1000:
-                rock.y = -150
-                rock = Rock(random.randint(0, 1440), -150)
+            if rock.y > 1500:
+                rock = Rock(random.randint(0, 1440), -250)
                 rock.change_size(random.randint(int(0.8), int(1.45)))
-            if rock2.y > 1000:
-                rock2 = Rock(random.randint(0, 1440), -150 - random.randint(0, 300))
+            if rock2.y > 1500:
+                rock2 = Rock(random.randint(0, 1440), -650 - random.randint(-75, 75))
                 rock2.change_size(random.randint(int(0.8), int(1.45)))
-            if rock3.y > 1000:
-                rock3.y = -150
-                rock3 = Rock2(random.randint(0, 1440), -200)
-            if rock4.y > 1000:
-                rock4 = Rock2(random.randint(0, 1440), -240 - random.randint(-20, 300))
+            if rock3.y > 1500:
+                rock3 = Rock2(random.randint(0, 1440), -250)
+                rock2.change_size(random.randint(int(0.8), int(1.45)))
+            if rock4.y > 1500:
+                rock4 = Rock2(random.randint(0, 1440), -250 - random.randint(-20, 300))
                 rock4.change_size(random.randint(int(0.8), int(1.45)))
         if going_left and going_forward and car.x > 0:
             car.move(car.x - 3, car.y)
         if going_right and going_forward and car.x < 1445:
             car.move(car.x + 3, car.y)
+        if going_backward:
+            rock.move(rock.x, rock.y - 3)
+            rock2.move(rock2.x, rock2.y - 3)
+            rock3.move(rock3.x, rock3.y - 3)
+            rock4.move(rock4.x, rock4.y - 3)
 
     else:
         screen.blit(display_message, (635, 160))
